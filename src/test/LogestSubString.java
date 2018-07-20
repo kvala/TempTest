cda151f7729fc079b5041e296c5bf0873c537397
Ketan Vala <ketan.vala@move.com>
2018-07-19 22:50:27 -0700

    return substring not just count

--- src/test/LogestSubString.java
+++ src/test/LogestSubString.java
@@ -12,25 +12,25 @@ public class LogestSubString {
 
 	public static void main(String[] args) {
 
-		List<Integer> myList = new ArrayList<>();
-		for (int i = 0; i < 100; i++)
-			myList.add(i);
+//		List<Integer> myList = new ArrayList<>();
+//		for (int i = 0; i < 100; i++)
+//			myList.add(i);
+//
+//		// sequential stream
+//		Stream<Integer> sequentialStream = myList.stream();
+//
+//		// parallel stream
+//		Stream<Integer> parallelStream = myList.parallelStream();
+//
+//		// using lambda with Stream API, filter example
+//		Stream<Integer> highNums = parallelStream.filter(p -> p > 90);
+//		// using lambda in forEach
+//		highNums.forEach(p -> System.out.println("High Nums parallel=" + p));
+//
+//		Stream<Integer> highNumsSeq = sequentialStream.filter(p -> p > 90);
+//		highNumsSeq.forEach(p -> System.out.println("High Nums sequential=" + p));
 
-		// sequential stream
-		Stream<Integer> sequentialStream = myList.stream();
-
-		// parallel stream
-		Stream<Integer> parallelStream = myList.parallelStream();
-
-		// using lambda with Stream API, filter example
-		Stream<Integer> highNums = parallelStream.filter(p -> p > 90);
-		// using lambda in forEach
-		highNums.forEach(p -> System.out.println("High Nums parallel=" + p));
-
-		Stream<Integer> highNumsSeq = sequentialStream.filter(p -> p > 90);
-		highNumsSeq.forEach(p -> System.out.println("High Nums sequential=" + p));
-
-		//System.out.println(lengthOfLongestSubstring2("abcabcdd"));
+		System.out.println(lengthOfLongestSubstrings("abcdaghijklmnog"));
 	}
 
 	public static int lengthOfLongestSubstring(String s) {
@@ -63,4 +63,22 @@ public class LogestSubString {
 		return ans;
 	}
 
+	public static String lengthOfLongestSubstrings(String s) {
+		String maxs = "";
+		int n = s.length();
+		Set<Character> set = new HashSet<>();
+		int ans = 0, i = 0, j = 0;
+		while (i < n && j < n) {
+			if (!set.contains(s.charAt(j))) {
+				set.add(s.charAt(j++));
+				if ((j - i) > ans) {
+					ans = j - i;
+					maxs = s.substring(i, j);
+				}
+			} else {
+				set.remove(s.charAt(i++));
+			}
+		}
+		return maxs;
+	}
 }
