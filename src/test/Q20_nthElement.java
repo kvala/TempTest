ccfdee4c65f82a2c823ae54eb76dd205ea03c62c
kvala <ketan.vala@gmail.com>
2023-05-08 12:07:03 -0700

    commit

---
+++ src/test/Q20_nthElement.java
@@ -0,0 +1,42 @@
+package test;
+
+public class Q20_nthElement {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null)
+            return null;
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        for (int i = 0; i < n; i++) {
+            //handle bad inputs such that n number of nodes are not there. 
+            if(fast == null){
+                return null;
+            }
+            
+            fast = fast.next;
+          
+        }
+
+        //if remove the first node
+        if (fast == null) {
+            head = head.next;
+            return head;
+        }
+
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        slow.next = slow.next.next;
+
+        return head;
+    }
+
+    public static void main(String[] args){
+        Q20_nthElement c = new Q20_nthElement();
+        ListNode input = new ListNode(1);
+        System.out.println(c.removeNthFromEnd(input, 5));
+    }
+}
