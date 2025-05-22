7d30ba7382fa0d4f20fc2bcd361988dbed5f36f2
kvala <ketan.vala@gmail.com>
2025-05-22 15:43:23 -0700

    Create Q1047_RemoveAllAdjDuplicates.java

---
+++ src/test/Q1047_RemoveAllAdjDuplicates.java
@@ -0,0 +1,23 @@
+public class Q1047_RemoveAllAdjDuplicates {
+     public String removeDuplicates(String s) {
+        StringBuilder sb = new StringBuilder();
+        int cnt = 0;
+        
+        for(char c : s.toCharArray() ){
+            
+            if(cnt != 0 && sb.charAt(cnt - 1) == c){
+                sb.deleteCharAt(cnt - 1);
+                cnt--;
+            }
+            else {
+                cnt++;
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+    public static void main(String[] args){
+        Q1047_RemoveAllAdjDuplicates s = new Q1047_RemoveAllAdjDuplicates();
+        System.out.println(s.removeDuplicates("abbaca"));
+    }
+}
