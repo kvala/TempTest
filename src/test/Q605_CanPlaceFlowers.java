43df42720c3dacec2e1a7e6c99907d6348cdbf05
kvala <ketan.vala@gmail.com>
2025-05-26 19:37:11 -0700

    Create Q605_CanPlaceFlowers.java

---
+++ src/test/Q605_CanPlaceFlowers.java
@@ -0,0 +1,25 @@
+public class Q605_CanPlaceFlowers {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int c = 0;
+
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (flowerbed[i] == 0) {
+                boolean emptyLeftPlot = (i == 0) || (flowerbed[i - 1] == 0);
+                boolean emptyRightPlot = (i == flowerbed.length - 1) || (flowerbed[i + 1] == 0);
+
+                if (emptyLeftPlot && emptyRightPlot) {
+                    c++;
+                }
+            }
+        }
+
+        return c >= n;
+
+    }
+
+    public static void main(String[] args) {
+        Q605_CanPlaceFlowers s = new Q605_CanPlaceFlowers();
+        int[] input = { 1, 0, 0, 0, 1 };
+        System.out.println(s.canPlaceFlowers(input, 1));
+    }
+}
