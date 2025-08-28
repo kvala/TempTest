e35bdfcb82b7cfef568e22f8da61ce4d204b1353
kvala <ketan.vala@gmail.com>
2025-08-28 15:48:20 -0700

    Update Q605_CanPlaceFlowers.java

--- src/test/Q605_CanPlaceFlowers.java
+++ src/test/Q605_CanPlaceFlowers.java
@@ -30,11 +30,11 @@ public class Q605_CanPlaceFlowers {
         if (n == 0)
           return true;
     
-        for (int i = 0; i < flowerbed.length; ++i)
+        for (int i = 0; i < flowerbed.length; i++)
           if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) &&
               (i == flowerbed.length - 1 || flowerbed[i + 1] == 0)) {
             flowerbed[i] = 1;
-            if (--n == 0)
+            if (n-- == 0)
               return true;
           }
     
