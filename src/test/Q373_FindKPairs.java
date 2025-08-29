28d0648eebd8a96335c8c79425966ca2a87ba054
kvala <ketan.vala@gmail.com>
2025-08-29 10:42:57 -0700

    Update Q373_FindKPairs.java

--- src/test/Q373_FindKPairs.java
+++ src/test/Q373_FindKPairs.java
@@ -24,7 +24,6 @@ public class Q373_FindKPairs {
     return ans;
   }
 
-
   public List<List<Integer>> kSmallestPairs2(int[] nums1, int[] nums2, int k) {
         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
         
@@ -36,12 +35,21 @@ public class Q373_FindKPairs {
         while (k-- > 0 && !pq.isEmpty()) {
             int[] arr = pq.poll();
             int i = arr[1], j = arr[2];
+            //Important to note we are taking i = arr[1] and j = arr[2] and checking j+1 pair one time. 
             res.add(Arrays.asList(nums1[i], nums2[j]));
             if (j + 1 < nums2.length) {
                 pq.add(new int[] { nums1[i] + nums2[j + 1], i, j + 1 });
             }
         }
         return res;
+    }
+
+
+    public static void main(String[] args){
+        Q373_FindKPairs s = new Q373_FindKPairs();
+        int[] i1 = {1,7,9,11};
+        int[] i2 = {2,4,6};
+        System.out.println(s.kSmallestPairs2(i1, i2, 2));
     }
      
 }
